8ea26c5ffd0edb60c669bff6761d11d828470998
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2023-04-29 16:52:26 +0300

    side menu

---
+++ src/app/main-page/side-menu/side-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css'],
+})
+export class SideMenuComponent implements OnInit {
+  @Input() lists: any;
+  isList: number;
+  isMenu: boolean = false;
+  isMenuBtn() {
+    this.isMenu = !this.isMenu;
+  }
+  isSearch: boolean = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
